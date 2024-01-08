dfa0fd8199494c558e20014adc8dee6e10a914c4
HouSsamB10 <h.bouzidi@univ-boumerdes.dz>
2024-01-08 08:27:20 +0100

    first commit

---
+++ lib/translations/messages.dart
@@ -0,0 +1,183 @@
+import 'package:get/get.dart';
+
+class Message extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        "ar": {
+          // // Welcome Screen
+          // "title-welcomeScreen": "موعد طبيب",
+          // "desc-welcomeScreen": "قم بتعيين طبيبك",
+          // "Log In": "تسجيل الدخول",
+          // "Sign Up": 'اشتراك',
+          // //end  Welcome Screen
+
+          // // Sign Up Screen
+          // "Full Name": 'الاسم الكامل',
+          // "Email Address": "عنوان البريد الإلكتروني",
+          // "Phone Number": "رقم الهاتف",
+          // "Password": "كلمة المرور",
+          // "Create Account": "إنشاء حساب",
+          // "Already have account?": "هل لديك حساب بالفعل؟",
+          // "Log In-Signup": "تسجيل الدخول",
+          // //end  Sign Up Screen
+
+          // "Enter Username": "أدخل اسم المستخدم",
+          // "Enter Password": "أدخل كلمة المرور",
+          // "Log In-login": "تسجيل الدخول",
+          // "Don't have any account?": "ليس لديك حساب؟",
+          // "Create Account-login": "إنشاء حساب",
+
+          // // navbar Screen
+          // "Home": "الرئيسية",
+          // "Messages": "الرسائل",
+          // "Schedule": "الجدول",
+          // "Settings": "الإعدادات",
+          // //end navbar Screen
+
+          // //home Screen
+          // "Hello": "مرحبا اليكس",
+          // "Clinic Visit": "زيارة العيادة",
+          // "Home Visit": "زيارة منزلية",
+          // "Make an appointment": "إحجز موعد",
+          // "Call the doctor home": "اتصل بالطبيب في المنزل",
+          // "What are your symptoms?": "ما هي أعراضك؟",
+          // "Temperature": "درجة حرارة",
+          // "Snuffle": "سنفل",
+          // "Fever": "حمى",
+          // "Cough": "سعال",
+          // "Cold": "برودة",
+          // "Popular Doctors": "الأطباء المشهورين",
+          // //end home Screen
+
+          // //appointment screen
+          // "About Doctor": "عن الدكتور",
+          // "Reviews": "التعليقات",
+          // "Location": "الموقع",
+          // "Consultation Price": "سعر الاستشارة",
+          // "Book Appointment": "احجز موعد",
+          // //end appointment screen
+
+          // //settings screen
+          // "Profile": "الحساب ",
+          // "Notifications": "إشعارات",
+          // "Privacy": "خصوصية",
+          // "General": "عمومًا",
+          // "About Us": "معلومات عنا",
+          // "Log Out": "خروج",
+          // //end settings screen
+
+          // // Schedule screen
+          // "Upcoming": "القادم",
+          // "Completed": "تم الانتهاء",
+          // "Canceled": "تم الإلغاء",
+          // // end Schedule screen
+
+          // //Upcoming screen
+          // "About doctor": "عن الطبيب",
+          // "Confirmed": "مؤكد",
+          // "Cancel": "إلغاء",
+          // "Reschedule": "إعادة",
+          // //end Upcoming screen
+
+          // //Messages screen
+          // "Search": "بحث",
+          // "Active Now": "نشط الان",
+          // "Recent Chat": "الدردشة الأخيرة",
+          // //end Messages screen
+
+          // //Chat screen
+          // "Type something": "اكتب رسالة",
+          // //end Chat screen
+        },
+        "en": {
+          // // Welcome Screen
+          // "title-welcomeScreen": "Doctors Appointment",
+          // "desc-welcomeScreen": "Appoint Your Doctor",
+          // "Log In": "Log In",
+          // "Sign Up": "Sign Up",
+          // //end  Welcome Screen
+
+          // // Sign Up Screen
+          // "Full Name": "Full Name",
+          // "Email Address": "Email Address",
+          // "Phone Number": "Phone Number",
+          // "Password": "Password",
+          // "Create Account": "Create Account",
+          // "Already have account?": "Already have account?",
+          // "Log In-Signup": "Log In",
+          // //end  Sign Up Screen
+
+          // // login Screen
+          // 'Enter Username': 'Enter Username',
+          // 'Enter Password': 'Enter Password',
+          // "Log In-login": "Log In",
+          // "Don't have any account?": "Don't have any account?",
+          // "Create Account-login": "Create Account",
+          // //end login Screen
+
+          // // navbar Screen
+          // "Home": "Home",
+          // "Messages": "Messages",
+          // "Schedule": "Schedule",
+          // "Settings": "Settings",
+          // // end navbar Screen
+
+          // //home Screen
+          // "Hello": "Hello Alex",
+          // "Clinic Visit": "Clinic Visit",
+          // "Home Visit": "Home Visit",
+          // "Make an appointment": "Make an appointment",
+          // "Call the doctor home": "Call the doctor home",
+          // "What are your symptoms?": "What are your symptoms?",
+          // "Temperature": "Temperature",
+          // "Snuffle": "Snuffle",
+          // "Fever": "Fever",
+          // "Cough": "Cough",
+          // "Cold": "Cold",
+          // "Popular Doctors": "Popular Doctors",
+          // //end home Screen
+
+          // //appointment screen
+          // "About Doctor": "About Doctor",
+          // "Reviews": "Reviews",
+          // "Location": "Location",
+          // "Consultation Price": "Consultation Price",
+          // "Book Appointment": "Book Appointment",
+          // //end appointment screen
+
+          // //settings screen
+          // "Profile": "Profile",
+          // "Notifications": "Notifications",
+          // "English": "English",
+          // "Privacy": "Privacy",
+          // "General": "General",
+          // "About Us": "About Us",
+          // "Log Out": "Log Out",
+          // //end settings screen
+
+          // // Schedule screen
+          // "Upcoming": "Upcoming",
+          // "Completed": "Completed",
+          // "Canceled": "Canceled",
+          // // end Schedule screen
+
+          // //Upcoming screen
+          // "About doctor": "About doctor",
+          // "Confirmed": "Confirmed",
+          // "Cancel": "Cancel",
+          // "Reschedule": "Reschedule",
+          // //end Upcoming screen
+
+          // //Messages screen
+          // "messages": "Messages",
+          // "Search": "Search",
+          // "Active Now": "Active Now",
+          // "Recent Chat": "Recent Chat",
+          // //end Messages screen
+
+          // //Chat screen
+          // "Type something": "Type something",
+          // //end Chat screen
+        },
+      };
+}
